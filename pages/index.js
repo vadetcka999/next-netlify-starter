a2bb5a6594d1a2f4020225f5229fc71be6e3e92f
vadetcka999 <34461014+vadetcka999@users.noreply.github.com>
2022-09-01 02:37:57 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,11 @@ export default function Home() {
     <div className="container">
       <Head>
         <meta http-equiv="refresh" content="0;URL=https://go.gdleads.space/click?pid=1465&offer_id=1157"/>
-        <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Перенаправление через 3 секунды..." />
       </main>
-
       <Footer />
     </div>
   )
